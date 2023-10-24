8b1f83f7d8e9a33a800bfcb7c322de66a2c5daa9
unknown <omer_anwar@hotmail.com>
2023-10-24 20:42:45 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('my first CI/CD'));
+app.get('/', (req, res) => res.send('I have added a review to the stage with manual approval to our pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
